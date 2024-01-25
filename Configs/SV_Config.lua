39da050c5bc19fcf4636853bb6019616e110bf5f
Diamond <urtabestemor@gmail.com>
2024-01-25 12:06:24 +0100

    Add support for multichar.

--- Configs/SV_Config.lua
+++ Configs/SV_Config.lua
@@ -1,3 +1,11 @@
 SVConfig = {}
 
 SVConfig.webhookURL = ""
+SVConfig.MultiCharESX = true
+SVConfig.Debug = true
+
+Debug = function(...)
+  if SVConfig.Debug then
+    print("[DEBUG] " .. ...)
+  end
+end
